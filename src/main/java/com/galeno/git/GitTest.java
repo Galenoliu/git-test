616707b4b035b295b2d5ef1ff10065f82f8e4e15
galeno <70539475@qq.com>
2021-07-22 15:09:25 +0800

    hotfix commit1

--- src/main/java/com/galeno/git/GitTest.java
+++ src/main/java/com/galeno/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("大爷的");
         System.out.println("heheda");
+        System.out.println("hellogit");
     }
 }
